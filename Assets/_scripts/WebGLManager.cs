1d212c943add223cb1bbb20a9cb7dd343adafba6
Botmasher <languagejosh@hotmail.com>
2018-04-20 09:26:32 -1000

    Disable webgl capturing all HTML page input

---
+++ Assets/_scripts/WebGLManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebGLManager : MonoBehaviour {
+
+	public bool captureAllPageInput = false;
+
+	void Start () {
+		// Set web player to grab all events when mouse is over it (true)
+		// or allow other page events like scrolling (false)
+		#if !UNITY_EDITOR && (UNITY_WEBGL || UNITY_WEBPLAYER)
+		
+			WebGLInput.captureAllKeyboardInput = captureAllPageInput;
+		
+		#endif
+	}
+
+}
